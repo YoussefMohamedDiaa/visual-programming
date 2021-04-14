5def2adfea72172f040a465465b8947c27052c1a
Ronic <abdoronic@gmail.com>
2021-04-15 01:24:41 +0200

    Reformat code
    Co-Authored-By: Mohamed Yasser Tarawa <47080017+MohamedTarawa@users.noreply.github.com>

--- app.js
+++ app.js
@@ -40,104 +40,127 @@ function createIndentation(indentLevel) {
 // }
 
 function convertBlockCommand(blockId, blocks) {
-  const block = blocks[blockId];
+    const block = blocks[blockId];
 
-  switch (block.opcode) {
-    case "event_whenflagclicked":
-      return "When Green Flage Clicked";
-    case "motion_movesteps":
-      return "Move " + block.inputs.STEPS[1][1] + " Steps";
-    case "motion_turnright":
-      return "Turn right " + block.inputs.DEGREES[1][1] + " Degrees";
-    case "motion_turnleft":
-      return "Turn left " + block.inputs.DEGREES[1][1] + " Degrees";
-    case "motion_gotoxy":
-      return "Go to " + block.inputs.X[1][1] + "," + block.inputs.Y[1][1];
-    case "motion_setx":
-      return "Set X to " + block.inputs.X[1][1];
-    case "motion_changexby":
-      return "Change X by " + block.inputs.DX[1][1];
-    case "motion_sety":
-      return "Set Y to " + block.inputs.Y[1][1];
-    case "motion_changeyby":
-      return "Change Y by " + block.inputs.DY[1][1];
-    case "looks_say":
-      return "Say " + block.inputs.MESSAGE[1][1];
-    case "looks_sayforsecs":
-      return (
-        "Say " + block.inputs.MESSAGE[1][1] + "For " + block.inputs.SECS[1][1]
-      );
-    case "looks_think":
-      return "Think " + block.inputs.MESSAGE[1][1];
-    case "looks_thinkforsecs":
-      return (
-        "Think " + block.inputs.MESSAGE[1][1] + "For " + block.inputs.SECS[1][1]
-      );
-    case "event_whenkeypressed":
-      return "When " + block.fields.KEY_OPTION[0] + " pressed";
-    case "control_forever":
-      return "Forever " + convertBlockCommand(block.inputs.SUBSTACK[1], blocks);
-    case "control_if":
-      return (
-        "If (" +
-        convertBlockCommand(block.inputs.CONDITION[1], blocks) +')'
-        // +
-        // ") Then: " +
-        // convertBlockCommand(block.inputs.SUBSTACK[1], blocks)
-      );
-    case "control_if_else":
-      return (
-        "If (" +
-        convertBlockCommand(block.inputs.CONDITION[1], blocks) +
-        ") Then: " +
-        convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
-        " Else: " +
-        convertBlockCommand(block.inputs.SUBSTACK2[1], blocks)
-      );
-    case "control_repeat":
-      return (
-        "Repeat (" +
-        convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
-        ") " +
-        block.inputs.TIMES[1][1] +
-        " Times"
-      );
-    case "control_wait":
-      return "Wait " + block.inputs.DURATION[1][1] + " seconds";
-    case "control_wait_until":
-      return (
-        "Wait until " + convertBlockCommand(block.inputs.CONDITION[1], blocks)
-      );
-    case "operator_gt":
-      return block.inputs.OPERAND1[1][1] + ">" + block.inputs.OPERAND2[1][1];
-    case "operator_lt":
-      return block.inputs.OPERAND1[1][1] + "<" + block.inputs.OPERAND2[1][1];
-    case "operator_equals":
-      return block.inputs.OPERAND1[1][1] + "=" + block.inputs.OPERAND2[1][1];
-    case "operator_and":
-      return (
-        '('+convertBlockCommand(block.inputs.OPERAND1[1], blocks) +
-        ") AND (" +
-        convertBlockCommand(block.inputs.OPERAND2[1], blocks)+')'
-      );
-    case "operator_or":
-      return (
-        '('+convertBlockCommand(block.inputs.OPERAND1[1], blocks) +
-        ") OR (" +
-        convertBlockCommand(block.inputs.OPERAND2[1], blocks)+')'
-      );
-    case "operator_not":
-      return (
-        "NOT(" + convertBlockCommand(block.inputs.OPERAND[1], blocks) + ")"
-      );
-    case "control_repeat_until":
-      return (
-        "Repeat (" +
-        convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
-        ") until " +
-        convertBlockCommand(block.inputs.CONDITION[1], blocks)
-      );
-  }
+    switch (block.opcode) {
+        case "event_whenflagclicked":
+            return "When Green Flage Clicked";
+        case "motion_movesteps":
+            return "Move " + block.inputs.STEPS[1][1] + " Steps";
+        case "motion_turnright":
+            return "Turn right " + block.inputs.DEGREES[1][1] + " Degrees";
+        case "motion_turnleft":
+            return "Turn left " + block.inputs.DEGREES[1][1] + " Degrees";
+        case "motion_gotoxy":
+            return "Go to " + block.inputs.X[1][1] + "," + block.inputs.Y[1][1];
+        case "motion_setx":
+            return "Set X to " + block.inputs.X[1][1];
+        case "motion_changexby":
+            return "Change X by " + block.inputs.DX[1][1];
+        case "motion_sety":
+            return "Set Y to " + block.inputs.Y[1][1];
+        case "motion_changeyby":
+            return "Change Y by " + block.inputs.DY[1][1];
+        case "looks_say":
+            return "Say " + block.inputs.MESSAGE[1][1];
+        case "looks_sayforsecs":
+            return (
+                "Say " +
+                block.inputs.MESSAGE[1][1] +
+                "For " +
+                block.inputs.SECS[1][1]
+            );
+        case "looks_think":
+            return "Think " + block.inputs.MESSAGE[1][1];
+        case "looks_thinkforsecs":
+            return (
+                "Think " +
+                block.inputs.MESSAGE[1][1] +
+                "For " +
+                block.inputs.SECS[1][1]
+            );
+        case "event_whenkeypressed":
+            return "When " + block.fields.KEY_OPTION[0] + " pressed";
+        case "control_forever":
+            return (
+                "Forever " +
+                convertBlockCommand(block.inputs.SUBSTACK[1], blocks)
+            );
+        case "control_if":
+            return (
+                "If (" +
+                convertBlockCommand(block.inputs.CONDITION[1], blocks) +
+                ")"
+                // +
+                // ") Then: " +
+                // convertBlockCommand(block.inputs.SUBSTACK[1], blocks)
+            );
+        case "control_if_else":
+            return (
+                "If (" +
+                convertBlockCommand(block.inputs.CONDITION[1], blocks) +
+                ") Then: " +
+                convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
+                " Else: " +
+                convertBlockCommand(block.inputs.SUBSTACK2[1], blocks)
+            );
+        case "control_repeat":
+            return (
+                "Repeat (" +
+                convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
+                ") " +
+                block.inputs.TIMES[1][1] +
+                " Times"
+            );
+        case "control_wait":
+            return "Wait " + block.inputs.DURATION[1][1] + " seconds";
+        case "control_wait_until":
+            return (
+                "Wait until " +
+                convertBlockCommand(block.inputs.CONDITION[1], blocks)
+            );
+        case "operator_gt":
+            return (
+                block.inputs.OPERAND1[1][1] + ">" + block.inputs.OPERAND2[1][1]
+            );
+        case "operator_lt":
+            return (
+                block.inputs.OPERAND1[1][1] + "<" + block.inputs.OPERAND2[1][1]
+            );
+        case "operator_equals":
+            return (
+                block.inputs.OPERAND1[1][1] + "=" + block.inputs.OPERAND2[1][1]
+            );
+        case "operator_and":
+            return (
+                "(" +
+                convertBlockCommand(block.inputs.OPERAND1[1], blocks) +
+                ") AND (" +
+                convertBlockCommand(block.inputs.OPERAND2[1], blocks) +
+                ")"
+            );
+        case "operator_or":
+            return (
+                "(" +
+                convertBlockCommand(block.inputs.OPERAND1[1], blocks) +
+                ") OR (" +
+                convertBlockCommand(block.inputs.OPERAND2[1], blocks) +
+                ")"
+            );
+        case "operator_not":
+            return (
+                "NOT(" +
+                convertBlockCommand(block.inputs.OPERAND[1], blocks) +
+                ")"
+            );
+        case "control_repeat_until":
+            return (
+                "Repeat (" +
+                convertBlockCommand(block.inputs.SUBSTACK[1], blocks) +
+                ") until " +
+                convertBlockCommand(block.inputs.CONDITION[1], blocks)
+            );
+    }
 }
 
 function addBeginCommandToPseudoCode(pseudoCode, indentLevel) {
