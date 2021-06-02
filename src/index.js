5cec1dd788f9d478ab125d04cb7547a01299a498
Ronic <abdoronic@gmail.com>
2021-06-02 13:45:52 +0200

    feat: add Visualization html + css layout
    Co-Authored-By: YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
    Co-Authored-By: Mohamed Adel <30552722+Moe98@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,43 +1,67 @@
-// Import Application class that is the main part of our PIXI project
 import { Application } from '@pixi/app'
 
-// In order that PIXI could render things we need to register appropriate plugins
-import { Renderer } from '@pixi/core' // Renderer is the class that is going to register plugins
+import { Renderer } from '@pixi/core'
 
-import { BatchRenderer } from '@pixi/core' // BatchRenderer is the "plugin" for drawing sprites
+import { BatchRenderer } from '@pixi/core'
 Renderer.registerPlugin('batch', BatchRenderer)
 
-import { TickerPlugin } from '@pixi/ticker' // TickerPlugin is the plugin for running an update loop (it's for the application class)
+import { TickerPlugin } from '@pixi/ticker'
 Application.registerPlugin(TickerPlugin)
 
-// And just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
 import { AppLoaderPlugin } from '@pixi/loaders'
 Application.registerPlugin(AppLoaderPlugin)
 
-// Sprite is our image on the stage
-import { Sprite } from '@pixi/sprite'
+import { Sprite } from '@pixi/Sprite'
 
-// App with width and height of the page
 const app = new Application({
-    width: window.innerWidth,
-    height: window.innerHeight
+    autoResize: true,
+    resolution: devicePixelRatio,
+    backgroundColor: 0x1099bb
 })
-document.body.appendChild(app.view) // Create Canvas tag in the body
+document.getElementById('canvas').appendChild(app.view)
 
-// Load the logo
+let catSprite = null
+
 app.loader.add('cat', './assets/cat.png')
 app.loader.load(() => {
-    const sprite = Sprite.from('cat')
-    sprite.scale.set(0.5, 0.5)
-    sprite.anchor.set(0.5) // We want to rotate our sprite relative to the center, so 0.5
-    app.stage.addChild(sprite)
+    catSprite = Sprite.from('cat')
+    catSprite.scale.set(0.2, 0.2)
+    catSprite.anchor.set(0.5)
 
-    // Position the sprite at the center of the stage
-    sprite.x = app.screen.width * 0.5
-    sprite.y = app.screen.height * 0.5
+    app.stage.addChild(catSprite)
 
-    // Put the rotating function into the update loop
+    catSprite.x = app.screen.width * 0.5
+    catSprite.y = app.screen.height * 0.5
+
     app.ticker.add((delta) => {
-        sprite.rotation += 0.02 * delta
+        catSprite.rotation += 0.02 * delta
     })
 })
+
+const inputElement = document.getElementById('scratchCodeInput')
+inputElement.addEventListener('change', handleFiles, false)
+
+function handleFiles() {
+    const codeFile = this.files[0]
+    const reader = new FileReader()
+    reader.onload = onReaderLoad
+    reader.readAsText(codeFile)
+}
+
+function onReaderLoad(event) {
+    const obj = JSON.parse(event.target.result)
+    alert(obj.name + ' ' + obj.version)
+}
+
+window.addEventListener('resize', resize)
+
+function resize() {
+    const parent = app.view.parentNode
+
+    app.renderer.resize(parent.clientWidth, parent.clientHeight)
+
+    if (catSprite)
+        catSprite.position.set(app.screen.width * 0.5, app.screen.height * 0.5)
+}
+
+resize()
