e651012d00d28212f8121e48a5a9c42df41d32e0
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-06-03 13:50:13 +0200

    feat: added looks command executor

--- src/executor.js
+++ src/executor.js
@@ -5,15 +5,35 @@ function execute(code, cat, dialogController) {
     const codeLines = getCodeLines(code)
     console.log(codeLines)
     console.log("#############")
-    const line = getLineNumber(codeLines,2)
+    const line = getLineNumber(codeLines,0)
+    //executeLooksCommand(line,dialogController)
     //executeMotionCommand(line, cat)
 
     //cat.x+=10
     //cat.y-=10
 }
 
+async function executeLooksCommand(command, dialogController){
+   if(command[command.length-2]=='For'){
+    //times looks command
+    if(command[0]=="Say")
+       dialogController.say(command.slice(1, command.length-2).join(" "))
+    if(command[0]=="Think")
+       dialogController.think(command.slice(1, command.length-2).join(" "))   
+    
+    await sleep(parseInt(command[command.length-1]));
+    dialogController.clear()
+    
+   }else{
+    if(command[0]=="Say")
+       dialogController.say(command.slice(1, command.length-2).join(" "))
+    if(command[0]=="Think")
+       dialogController.think(command.slice(1, command.length-2).join(" "))   
+   }
+}
+
 function executeMotionCommand(command, cat){
-    console.log("command"+command[0])
+    
     switch(command[0]) {
         case "Move":
           cat.x += parseInt(command[1]);
@@ -44,6 +64,12 @@ function executeMotionCommand(command, cat){
            break;
       }
 }
+
+
+function sleep(s) {
+    let ms = s*1000
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 function getCodeLines(code){
     const codeLines = code.split('\n')
