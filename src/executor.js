d21686bf6d95d3bdf1bf82d4a45b512ee63e0d02
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-06-03 12:37:51 +0200

    feat: execute motion commands function added

--- src/executor.js
+++ src/executor.js
@@ -5,9 +5,44 @@ function execute(code, cat, dialogController) {
     const codeLines = getCodeLines(code)
     console.log(codeLines)
     console.log("#############")
-    console.log(getLineNumber(codeLines,3))
+    const line = getLineNumber(codeLines,2)
+    //executeMotionCommand(line, cat)
+
     //cat.x+=10
     //cat.y-=10
+}
+
+function executeMotionCommand(command, cat){
+    console.log("command"+command[0])
+    switch(command[0]) {
+        case "Move":
+          cat.x += parseInt(command[1]);
+          break;
+        case "Turn":
+          if(command[1]=="right")
+            cat.rotation +=parseInt(command[2]);
+          else
+            cat.rotation -=parseInt(command[2]);   
+          break;
+        case "Change":
+          if(command[1]=="X")  
+            cat.x += parseInt(command[3]);
+          else
+            cat.y += parseInt(command[3]);  
+          break;
+        case "Set":
+          if(command[1]=="X")  
+            cat.x = parseInt(command[3]);
+          else
+            cat.y = parseInt(command[3]);  
+          break;
+        case "Go":
+            cat.x = parseInt(command[2].split(',')[0]);
+            cat.y = parseInt(command[2].split(',')[1]);
+            break;     
+        default:
+           break;
+      }
 }
 
 function getCodeLines(code){
