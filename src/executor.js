47f24903ebf980f192ec2c7dc887f9b177f1996f
YoussefMohamedDiaa <youssefmohameddiaaeldin@gmail.com>
2021-06-03 11:13:03 +0200

    added line splitter and getting certain line as an array in executor

--- src/executor.js
+++ src/executor.js
@@ -1,6 +1,22 @@
 function execute(code, cat, dialogController) {
     console.log('Executing:')
     console.log(code)
+    console.log("#############")
+    const codeLines = getCodeLines(code)
+    console.log(codeLines)
+    console.log("#############")
+    console.log(getLineNumber(codeLines,3))
+    //cat.x+=10
+    //cat.y-=10
+}
+
+function getCodeLines(code){
+    const codeLines = code.split('\n')
+    return codeLines
+}
+
+function getLineNumber(codeLines, idx){
+    return codeLines[idx].trim().split(" ")
 }
 
 export default execute
